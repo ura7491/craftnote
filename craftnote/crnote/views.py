5a4166015982caa666070077a52de30f0a85dd83
Iuriy Demenev <ura7491@gmail.com>
2024-04-08 22:35:00 +0300

    Добавил регистрацию пользователя

--- craftnote/crnote/views.py
+++ craftnote/crnote/views.py
@@ -1,6 +1,17 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 def signupuser(request):
-
-    return render(request, 'crnote/signupuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'crnote/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+            except IntegrityError:
+                return render(request, 'crnote/signupuser.html', {'form':UserCreationForm(), 'error':'Данный пользователь уже существует'})
+        else:
+            return render(request, 'crnote/signupuser.html', {'form':UserCreationForm(), 'error':'Введенные пароли не совпадают'})
