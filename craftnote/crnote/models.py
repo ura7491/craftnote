db5f9a7a0fd8f52b02c075cb44b243171ccd5db7
Iuriy Demenev <ura7491@gmail.com>
2024-05-06 22:08:18 +0300

    Created models Task and added on page admin

--- craftnote/crnote/models.py
+++ craftnote/crnote/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    #Заголовок.Максимальная длина заголовка
+    Заголовок = models.CharField(max_length=100)
+
+    #Описание. Атрибут задает необязательное поле
+    Описание = models.TextField(blank=True)
+
+    #Дата и время создания записи. Атрибут не позволит менять дату вручную
+    Дата_создания = models.DateTimeField(auto_now_add=True)
+
+    #Дата и время выполненной задачи
+    Дата_выполнения = models.DateTimeField(null=True, blank=True)
+
+    #Галочка о важной задачи
+    Важно = models.BooleanField(default=False)
+
+    #Привязка задачи к пользователю. ВАЖНО!
+    Пользователь = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #Отобразение заголовка на странице
+    def __str__(self):
+        return self.Заголовок
